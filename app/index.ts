997e31da6a6f6310b9c2af2e596e60419252ba37
Szymon Bultrowicz <szymonbultrowicz@users.noreply.github.com>
2019-03-10 21:42:42 +0100

    Heart icon + typescript support (#3)

--- app/index.ts
+++ app/index.ts
@@ -14,27 +14,33 @@ const minutesLabel = document.getElementById("minutes");
 const hrLabel = document.getElementById("hr");
 const dateLabel = document.getElementById("date");
 
+const setText = (el: Element | null, text: string): void => {
+  if (el !== null) {
+    el.text = text;
+  }
+}
+
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
   const today = evt.date;
-  const hours = today.getHours();
+  let hours: string;;
   if (preferences.clockDisplay === "12h") {
     // 12h format
-    hours = hours % 12 || 12;
+    hours = `${today.getHours() % 12 || 12}`;
   } else {
     // 24h format
-    hours = zeroPad(hours);
+    hours = zeroPad(today.getHours());
   }
   let mins = zeroPad(today.getMinutes());
-  hourLabel.text = `${hours}`;
-  minutesLabel.text = `${mins}`;
-  dateLabel.text = formatDate(today);
+  setText(hourLabel, `${hours}`);
+  setText(minutesLabel, `${mins}`);
+  setText(dateLabel, formatDate(today));
 }
 
 const hrm = new HeartRateSensor();
 hrm.onreading = () => {
   console.log(hrm.heartRate);
-  hrLabel.text = `${hrm.heartRate}`;
+  setText(hrLabel, `${hrm.heartRate}`);
 };
 if (display.on) {
   hrm.start();

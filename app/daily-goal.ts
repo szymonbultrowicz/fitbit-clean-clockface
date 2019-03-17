9afad1d34cf5dcd1200c4ec129c10ce65e352d4e
Szymon Bultrowicz <szymon.bultrowicz@gmail.com>
2019-03-17 22:22:13 +0100

    Battery status + steps count

---
+++ app/daily-goal.ts
@@ -0,0 +1,36 @@
+import { today } from "user-activity";
+import { me } from "appbit";
+
+export enum GoalType {
+    activeMinutes,
+    calories,
+    distance,
+    elevationGain,
+    steps,
+};
+
+export class Goal {
+    public type: GoalType | undefined;
+    public get value(): number | undefined {
+        if (!me.permissions.granted("access_activity")) {
+            return undefined;
+        }
+        const activity = today.adjusted;
+        switch (this.type) {
+            case GoalType.activeMinutes:
+                return activity.activeMinutes;
+            case GoalType.calories:
+                return activity.calories;
+            case GoalType.distance:
+                return activity.distance;
+            case GoalType.elevationGain:
+                return activity.elevationGain;
+            case GoalType.steps:
+                return activity.steps;
+            default:
+                return undefined;
+        }
+    }
+}
+
+export const goal = new Goal();

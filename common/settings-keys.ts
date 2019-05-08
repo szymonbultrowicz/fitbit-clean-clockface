33dfbad66c5f0aa3d4db4543ca763ffd7b685ffc
Szymon Bultrowicz <szymonbultrowicz@users.noreply.github.com>
2019-05-08 22:03:52 +0200

    HR reading timeout (#13)

--- common/settings-keys.ts
+++ common/settings-keys.ts
@@ -2,4 +2,5 @@ export const enum SettingsKeys {
   ENABLE_GOALS = "enableGoals",
   ENABLED_GOAL = "enabledGoal",
   ENABLE_BATTERY = "enableBattery",
+  HR_TIMEOUT = "hrTimeout",
 }
--- companion/coercion.ts
+++ companion/coercion.ts
@@ -1,5 +1,6 @@
 import { GoalType } from "../common/goal-type";
 import { goalsOptions } from "../common/goals-options";
+import { hrTimeoutOptions } from "../common/hr-timeout-options";
 import { SettingsKeys } from "../common/settings-keys";
 import { SelectValue } from "./select-value";
 
@@ -14,32 +15,36 @@ export function stringifyBoolean(value: boolean) {
 export function coerceSettingsValue(
   key: SettingsKeys,
   value: string,
-): boolean | GoalType | null {
+): boolean | number | GoalType | null {
   switch (key) {
     case SettingsKeys.ENABLE_GOALS:
     case SettingsKeys.ENABLE_BATTERY:
       return coerceBoolean(value);
     case SettingsKeys.ENABLED_GOAL:
-      return coerceGoalsValue(value);
+      return coerceSelectionValue<GoalType>(value);
+    case SettingsKeys.HR_TIMEOUT:
+      return coerceSelectionValue<number>(value);
   }
   return undefined;
 }
 
 export function stringifySettingsValue(
   key: SettingsKeys,
-  value: boolean | GoalType,
+  value: boolean | number | GoalType,
 ): string | undefined {
   switch (key) {
     case SettingsKeys.ENABLE_GOALS:
     case SettingsKeys.ENABLE_BATTERY:
       return stringifyBoolean(value as boolean);
     case SettingsKeys.ENABLED_GOAL:
-      return stringifyGoalsValue(value as GoalType);
+      return stringifySelectionValue(value as GoalType, goalsOptions);
+    case SettingsKeys.HR_TIMEOUT:
+      return stringifySelectionValue(value as number, hrTimeoutOptions);
   }
   return undefined;
 }
 
-export function coerceGoalsValue(value: string) {
+export function coerceSelectionValue<T>(value: string): T | undefined {
   const selectValue = JSON.parse(value) as SelectValue;
   if (selectValue.values.length === 1) {
     return selectValue.values[0].value;
@@ -49,14 +54,14 @@ export function coerceGoalsValue(value: string) {
   return undefined;
 }
 
-export function stringifyGoalsValue(value: GoalType) {
-  return JSON.stringify(getGoalsSelection(value));
+export function stringifySelectionValue<T>(value: T, options: SelectOption[]) {
+  return JSON.stringify(getSelection(value, options));
 }
 
-function getGoalsSelection(value: GoalType): SelectValue {
-  const selected = goalsOptions.findIndex((o) => o.value === value);
+function getSelection<T>(value: T, options: SelectOption[]): SelectValue {
+  const selected = options.findIndex((o) => o.value === value);
   return {
     selected: [selected],
-    values: goalsOptions,
+    values: options,
   };
 }
